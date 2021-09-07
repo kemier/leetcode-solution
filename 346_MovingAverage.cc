e064fbb4230d5189ffc26c3c24175d0a973d6319
kemier <zengningbj@163.com>
2021-09-07 18:46:37 +0800

    leetcode 345-347

---
+++ 346_MovingAverage.cc
@@ -0,0 +1,29 @@
+#include <queue>
+
+using namespace std;
+
+class MovingAverage
+{
+    queue<int> q;
+    int sum = 0;
+    int cap;
+
+public:
+    /** Initialize your data structure here. */
+    MovingAverage(int size)
+    {
+        cap = size;
+    }
+
+    double next(int val)
+    {
+        sum += val;
+        q.push(val);
+        if (q.size() > cap)
+        {
+            sum -= q.front();
+            q.pop();
+        }
+        return sum / double(q.size());
+    }
+};
