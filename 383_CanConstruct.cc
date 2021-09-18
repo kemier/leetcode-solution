e77727f993120a90d4d40bce612f63650971b6c4
kemier <zengningbj@163.com>
2021-09-18 20:57:32 +0800

    leetcode 382-383

---
+++ 383_CanConstruct.cc
@@ -0,0 +1,50 @@
+#include <string>
+
+using namespace std;
+
+
+// 时间复杂度: O(n^2)
+// 空间复杂度：O(1)
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        for (int i = 0; i < magazine.length(); i++) {
+            for (int j = 0; j < ransomNote.length(); j++) {
+                // 在ransomNote中找到和magazine相同的字符
+                if (magazine[i] == ransomNote[j]) {
+                    ransomNote.erase(ransomNote.begin() + j); // ransomNote删除这个字符
+                    break;
+                }
+            }
+        }
+        // 如果ransomNote为空，则说明magazine的字符可以组成ransomNote
+        if (ransomNote.length() == 0) {
+            return true;
+        }
+        return false;
+    }
+};
+
+
+// 时间复杂度: O(n)
+// 空间复杂度：O(1)
+class Solution2 {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int record[26] = {0};
+        for (int i = 0; i < magazine.length(); i++) {
+            // 通过recode数据记录 magazine里各个字符出现次数
+            record[magazine[i]-'a'] ++;
+        }
+        for (int j = 0; j < ransomNote.length(); j++) {
+            // 遍历ransomNote，在record里对应的字符个数做--操作
+            record[ransomNote[j]-'a']--;
+            // 如果小于零说明ransomNote里出现的字符，magazine没有
+            if(record[ransomNote[j]-'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
