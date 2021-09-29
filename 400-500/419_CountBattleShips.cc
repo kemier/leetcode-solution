d668ed01a58857adf06f3d793845a4751429baff
kemier <zengningbj@163.com>
2021-09-29 23:54:14 +0800

    leetcode 418-421

---
+++ 400-500/419_CountBattleShips.cc
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int countBattleships(vector<vector<char>> &board)
+    {
+        int nCount = 0;
+        for (int i = 0; i < board.size(); ++i)
+        {
+            for (int j = 0; j < board[i].size(); ++j)
+            {
+                if (board[i][j] == 'X')
+                {
+                    if (i > 0 && board[i - 1][j] == 'X' ||
+                        j > 0 && board[i][j - 1] == 'X')
+                        continue;
+                    ++nCount;
+                }
+            }
+        }
+        return nCount;
+    }
+};
